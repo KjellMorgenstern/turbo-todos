8bdb51ed596e442ec4911d0d1a342a23f9e3f84b
Kjell Morgenstern <kjell@fritzing.org>
2022-08-30 18:28:04 +0200

    Enable basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action :http_basic
+  
+  def http_basic(realm: nil)
+      http_basic_authenticate_or_request_with name: 'theuser', password: 'todo', realm: realm
+  end
 end
